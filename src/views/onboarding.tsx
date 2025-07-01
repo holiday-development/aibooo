c199a5743bc666347088e754fb490e6d49f8ea01
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2025-07-02 08:01:56 +0900

    feat(onboarding): implement onboarding carousel for enhanced user experience

--- src/views/onboarding.tsx
+++ src/views/onboarding.tsx
@@ -1,16 +1,20 @@
 import { Button } from '@/components/ui/button';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 import { XIcon } from 'lucide-react';
 
 export function Onboarding() {
   return (
-    <div className="h-full w-full p-8 flex flex-col">
+    <div className="h-full w-full">
       <div className="absolute top-2 right-2">
         <CloseButton />
       </div>
-      <h1 className="text-3xl">あなたの Aibooo</h1>
-      <div className="h-[120px] flex justify-center mt-12 mx-auto">
-        <img src="/ai.svg" alt="ai" />
-      </div>
+      <OnboardingCarousel />
     </div>
   );
 }
@@ -20,5 +24,29 @@ function CloseButton() {
     <Button size="icon" variant="ghost">
       <XIcon width={24} height={24} className="text-gray-400" />
     </Button>
+  );
+}
+
+function OnboardingCarousel() {
+  return (
+    <Carousel className="w-full h-full p-8">
+      <CarouselContent>
+        <CarouselItem>
+          <h1 className="text-3xl">あなたの Aibooo</h1>
+          <div className="h-[120px] flex justify-center mt-12 mx-auto">
+            <img src="/ai.svg" alt="ai" />
+          </div>
+        </CarouselItem>
+        <CarouselItem>
+          <div>2</div>
+        </CarouselItem>
+      </CarouselContent>
+      <div className="absolute bottom-4 left-10 z-10">
+        <CarouselPrevious />
+      </div>
+      <div className="absolute bottom-4 right-10 z-10">
+        <CarouselNext />
+      </div>
+    </Carousel>
   );
 }
